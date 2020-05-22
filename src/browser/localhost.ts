5d6e1593cba701486e40795ac418985a162df01e
Brendan McGuire <brendan@bren.app>
2020-05-21 23:12:29 -0400

    New Localhost impl

--- src/browser/localhost.ts
+++ src/browser/localhost.ts
@@ -1,35 +1,78 @@
 import Store from "../core/Store";
 
-export default class LocalStoreStore extends Store {
-  async initalize() {
+export default class LocalStoreStore<T> extends Store<T> {
+  prefix: string = "";
+  async prepare() {
     let stores = await LocalStoreStore.stores();
 
     if (!stores.includes(this.name)) {
       stores.push(this.name);
       localStorage.setItem("keya-localhost-stores", stores.join(","));
     }
+
+    this.prefix = `keya-store-${this.name}-v${this.version}-`;
   }
 
-  // Hardware interactions
-  store: { [key: string]: any } = {};
-  async load(): Promise<void> {
-    Object.keys(localStorage)
-      .filter(key => key.startsWith(`keya-store-${this.name}-v${this.version}`))
-      .forEach(key => {
-        this.store[key] = JSON.parse(`${localStorage.getItem(key)}`);
-      });
-  }
-  async save(): Promise<void> {
-    let keys = Object.keys(this.store);
+  /**
+   * Gets all keys in the store
+   */
+  async all() {
+    const keys = Object.keys(localStorage).filter((key) =>
+      key.startsWith(this.prefix)
+    );
 
-    keys.forEach(key => {
-      localStorage.setItem(
-        `keya-store-${this.name}-v${this.version}-${key}`,
-        JSON.stringify(this.store[key])
-      );
-    });
+    return keys.map((key) => ({
+      key,
+      value: (localStorage.getItem("key") as unknown) as T,
+    }));
   }
 
+  /**
+   * Gets a particular key in the store
+   * @param key to get
+   */
+  async get(key: string): Promise<T | null> {
+    const value = localStorage.getItem(this.prefix + key);
+
+    if (!value) {
+      return null;
+    }
+
+    return this.hydrate(value);
+  }
+
+  /**
+   * Sets a particular key in the store
+   * @param key to set
+   * @param value to set it to
+   */
+  async set(key: string, value: T): Promise<boolean> {
+    const encoded = await this.stringify(value);
+
+    localStorage.setItem(this.prefix + key, encoded);
+    return true;
+  }
+
+  /**
+   * Deletes a specified key from the store
+   * @param key to delete
+   */
+  async delete(key: string): Promise<boolean> {
+    localStorage.removeItem(this.prefix + key);
+    return true;
+  }
+
+  /**
+   * Checks if they key has a value in the store
+   * @param key to check
+   */
+  async has(key: string): Promise<boolean> {
+    return localStorage.hasOwnProperty(this.prefix + key);
+  }
+
+  /**
+   * Gets all stores
+   */
   static async stores() {
     return (localStorage.getItem("keya-localhost-stores") || "").split(",");
   }
