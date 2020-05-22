a3d7cf35c8da5ec6d89953c7ec6f0d9e86bfe1e9
Brendan McGuire <brendan@bren.app>
2020-05-21 23:11:31 -0400

    Remove out from repo

--- out/browser/main.d.ts
+++
@@ -1,4 +0,0 @@
-import IndexedDBStore from "./indexeddb";
-import LocalStorageStore from "./localhost";
-export declare function store(name: string): Promise<IndexedDBStore | LocalStorageStore>;
-export declare function stores(): Promise<string[]>;
--- out/node/main.d.ts
+++
@@ -1,3 +0,0 @@
-import SQLiteStore from "./sqlite";
-export declare function store(name: string): Promise<SQLiteStore>;
-export declare function stores(): Promise<string[]>;
