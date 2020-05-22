0fc8a2486a09b66f2217c101aa4d8ffe8abafc34
Brendan McGuire <brendan@bren.app>
2020-05-21 23:12:02 -0400

    New abstract store

--- src/core/Store.ts
+++ src/core/Store.ts
@@ -1,66 +1,69 @@
 /**
  * Represents a specific Keya Store
  * Implemented by browser/node implementations
+ *
+ * Basically, this extends a Map, without doing so (because async operations mess it up)
  */
 
-export default abstract class Store {
+export type HydrateFunction<T> = (stored: string) => Promise<T> | T;
+export type StringifyFunction<T> = (prepared: T) => Promise<string> | string;
+
+export default abstract class Store<T> {
   name: string;
   version: number = 0;
 
-  abstract store: { [key: string]: any };
-
-  // API Implementation, overwritten by local implementations (but commonalities like find can be preserved)
-  async get(key: string) {
-    await this.load();
-    return this.store[key];
-  }
-
-  async set(key: string, value: any) {
-    await this.load();
-    this.store[key] = value;
-
-    try {
-      await this.save();
-      return true;
-    } catch (e) {
-      return false;
-    }
-  }
-
-  async delete(key: string) {
-    await this.load();
-    return delete this.store[key];
-  }
-
-  async all() {
-    await this.load();
-    return Object.keys(this.store).map(key => ({
-      key,
-      value: this.store[key]
-    }));
-  }
-
-  async find(finder: (value: any, name: string) => Promise<boolean> | boolean) {
-    let all = await this.all();
-    let include = await Promise.all(
-      all.map(({ value, key }) => finder(value, key))
-    );
-
-    return all.filter((v, i, a) => include[i]);
-  }
-
-  async clear() {
-    return this.all().then(keys =>
-      keys.forEach(record => this.delete(record.key))
-    );
-  }
-
-  // Lifecycle
-  abstract async initalize(): Promise<void>;
-  abstract async load(): Promise<void>;
-  abstract async save(): Promise<void>;
+  hydrate: HydrateFunction<T> = JSON.parse;
+  stringify: StringifyFunction<T> = JSON.stringify;
 
   constructor(name: string) {
     this.name = name;
+  }
+
+  abstract async prepare(): Promise<void>;
+
+  // Access
+  abstract async all(): Promise<
+    {
+      key: string;
+      value: T;
+    }[]
+  >;
+  abstract async get(key: string): Promise<T | null>;
+  abstract async set(key: string, value: T): Promise<boolean>;
+  abstract async delete(key: string): Promise<boolean>;
+  abstract async has(key: string): Promise<boolean>;
+
+  /**
+   * Deletes all elements from a store
+   */
+  async clear() {
+    const records = await this.all();
+
+    return Promise.all(records.map(({ key }) => this.delete(key)));
+  }
+
+  /**
+   * Iterates through each element
+   * @param callback Handles each function
+   */
+  async forEach(callback: (value: T, key: string, store: Store<T>) => void) {
+    const records = await this.all();
+    records.forEach(({ key, value }) => callback(value, key, this));
+  }
+
+  /**
+   * Async Iterator
+   *
+   * for await (const { key, value } in store) {
+   *  console.log(key, value)
+   * }
+   *
+   */
+  async *[Symbol.asyncIterator]() {
+    const records = await this.all();
+
+    for (const record of records) {
+      yield record;
+    }
   }
 }
