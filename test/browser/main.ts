407264d00b44aa7e221d852dd37d7b629e125f0f
Brendan McGuire <brendan@bren.app>
2020-05-21 23:12:55 -0400

    Better testing

--- test/browser/main.ts
+++ test/browser/main.ts
@@ -1,11 +1,27 @@
 import * as keya from "../../src/browser/main";
 
-(async function() {
-  const store = await keya.store("vexdb");
+(async function () {
+  const store = await keya.store<Date>("dates");
 
-  await store.set("latest", { key: 1234 });
+  // Set the processing functions (for when using non-object datatypes)
+  store.hydrate = (string) => new Date(Number.parseInt(string));
+  store.stringify = (date) => date.getTime().toString();
 
-  console.log(await store.get("latest"));
+  // Set data
+  console.log("set");
+  await store.set("time", new Date());
 
-  console.log(store);
+  // All
+  console.log("all");
+  const all = await store.all();
+  console.log(all);
+
+  // Get
+  console.log("get");
+  const value = await store.get("time");
+  console.log(value);
+
+  // Delete
+  console.log("delete");
+  store.delete("time").then(console.log);
 })();
